35335c2e40866bbe6dc61014c2cdbb165c1ff33e
cykuo <cykuotw45@gmail.com>
2023-11-19 14:52:00 -0500

    [fix] change migrate to mysql

--- web/migrate/data/000001_init.up.sql
+++ web/migrate/data/000001_init.up.sql
@@ -1,43 +1,45 @@
 CREATE TABLE IF NOT EXISTS stock_price (
-    id uuid PRIMARY KEY,
-    company_id VARCHAR NOT NULL,
-    update_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    id SERIAL PRIMARY KEY,
+    uuid VARCHAR(36),
+    company_id VARCHAR(10) NOT NULL,
+    update_date DATETIME NOT NULL,
     price_date DATE NOT NULL,
-    open INTEGER NOT NULL CHECK (open>0),
-    close INTEGER NOT NULL CHECK (close>0),
-    high INTEGER NOT NULL CHECK (high>0),
-    low INTEGER NOT NULL CHECK (low>0),
-    change INTEGER NOT NULL,
-    change_percent INTEGER NOT NULL,
-    volume BIGINT NOT NULL CHECK (volume>0),
-    amount BIGINT NOT NULL CHECK (amount>0)
+    open INT UNSIGNED NOT NULL,
+    close INT UNSIGNED NOT NULL,
+    high INT UNSIGNED NOT NULL,
+    low INT UNSIGNED NOT NULL,
+    price_change INT NOT NULL,
+    change_percent INT NOT NULL,
+    volume BIGINT UNSIGNED NOT NULL,
+    amount BIGINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS future_price (
-    id uuid PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    uuid VARCHAR(36),
     contract_id VARCHAR(4) NOT NULL,
     contract_date VARCHAR(7) NOT NULL,
-    update_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    update_date DATETIME NOT NULL,
     price_date DATE NOT NULL,
-    open INTEGER NOT NULL CHECK (open>0),
-    close INTEGER NOT NULL CHECK (close>0),
-    high INTEGER NOT NULL CHECK (high>0),
-    low INTEGER NOT NULL CHECK (low>0),
-    volume BIGINT NOT NULL CHECK (volume>0)
+    open INT UNSIGNED NOT NULL,
+    close INT UNSIGNED NOT NULL,
+    high INT UNSIGNED NOT NULL,
+    low INT UNSIGNED NOT NULL,
+    volume BIGINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS option_oi (
-    id uuid PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    uuid VARCHAR(36),
     contract_id VARCHAR(4) NOT NULL,
     contract_date VARCHAR(7) NOT NULL,
-    update_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    update_date DATETIME NOT NULL,
     type CHAR(1) NOT NULL,
     strike_price SMALLINT NOT NULL,
-    open INTEGER NOT NULL CHECK (open>0),
-    close INTEGER NOT NULL CHECK (close>0),
-    high INTEGER NOT NULL CHECK (high>0),
-    low INTEGER NOT NULL CHECK (low>0),
-    volume BIGINT NOT NULL CHECK (volume>0),
-    open_interest INTEGER NOT NULL CHECK (open_interest>0),
-    settle_price INTEGER
+    open INT UNSIGNED NOT NULL,
+    close INT UNSIGNED NOT NULL,
+    high INT UNSIGNED NOT NULL,
+    low INT UNSIGNED NOT NULL,
+    open_interest INT UNSIGNED NOT NULL,
+    settle_price INT UNSIGNED
 );
